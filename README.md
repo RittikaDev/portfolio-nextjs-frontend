bdf10af1a026bc124e8d4da30cfc096adcb52dec
RittikaDev <rittika.dev@gmail.com>
2025-02-10 23:17:59 +0600

    feat: README add

--- README.md
+++ README.md
@@ -1,7 +1,60 @@
-# Next.Js-Authentication-Starter-Pack
+# Project Portfolio
 
-## Installation:
+This is a personal project portfolio built using Next.Js mainly. It showcases various projects, with a focus on providing a smooth and interactive user experience through UI components and dynamic content loading.
 
-1. Clone the repository.
-2. Install dependencies using `npm install`.
-3. Run the project using `npm run dev`.
+## Features
+
+- **Project Showcase**: Each project includes a title, description, links to GitHub repositories (frontend & backend), deployment links, and a list of technologies used.
+- **Responsive Design**: The portfolio is responsive and works across different screen sizes, ensuring a great user experience on both mobile and desktop devices.
+- **Image Gallery**: An interactive image gallery displays snapshots of each project.
+- **Shiny Button**: A shiny, animated button that allows users to download your resume.
+- **Animated Title & Content**: Titles and content come to life with animated effects.
+- **Tech Stack Information**: The portfolio dynamically shows the frontend and backend tech stack used for each project.
+- **Background Image**: The header section includes a background image with a low opacity effect, providing a visually appealing look.
+
+## Technologies Used
+
+### Frontend:
+
+- **React**: JavaScript library for building user interfaces.
+- **Tailwind CSS**: Utility-first CSS framework for styling.
+- **Lucide**: Icon library for modern and lightweight icons.
+- **Next.js**: Framework for server-side rendering and static site generation.
+- **Shiny Button**: Custom component with an animated shiny effect.
+- **TypeScript**: For type safety and better development experience.
+
+## Installation
+
+To run the portfolio locally, follow these steps:
+
+1. Clone this repository:
+
+   ```bash
+   git clone https://github.com/RittikaDev/portfolio-nextjs-frontend
+   cd portfolio
+   ```
+
+- **Install the dependencies**:
+
+  ```bash
+  npm install
+  ```
+
+- **Create a `.env` file in the root directory of the project with the following environment variables**:
+
+  ```env
+  GITHUB_ID=
+  GITHUB_SECRET=
+  GOOGLE_ID=
+  GOOGLE_SECRET=GOCSPX-
+  NEXTAUTH_SECRET=
+  API_BASE_URL=
+  ```
+
+- **Start the development server**:
+
+  ```bash
+  npm run dev
+  ```
+
+- **Visit** `http://localhost:3000` **in your browser to see the portfolio**.
--- src/components/Home/AboutMe.tsx
+++ src/components/Home/AboutMe.tsx
@@ -3,64 +3,57 @@ import { TextHoverEffect } from "@/components/ui/text-hover-effect";
 import ShinyButton from "../ui/shiny-button";
 
 const AboutMe = () => {
-	return (
-		<section className="relative md:pt-10 md:pb-56 2xl:pb-28 dark:bg-darkTheme">
-			<div className="hidden md:block">
-				<Spotlight
-					className="-top-40 left-0 md:left-60 md:top-40"
-					fill="white"
-				/>
-			</div>
-			<div className="hidden md:block">
-				<TextHoverEffect text="ABOUT" />
-			</div>
-			<div className="w-full md:absolute md:top-0 lg:translate-y-[50%]">
-				<div className="flex justify-center">
-					<div className="md:col-span-4 space-y-6 text-center md:text-left">
-						<div className="space-y-4 flex flex-col items-center text-center">
-							<h1 className="font-bold text-4xl md:text-5xl pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-teal-900 to-teal-400 bg-clip-text leading-none text-transparent dark:from-white dark:to-slate-900/10">
-								About Me
-							</h1>
-							<p className="text-muted-foreground leading-relaxed max-w-2xl">
-								I am a passionate Full-Stack Developer with a strong focus on
-								building responsive, high-performance web applications. My
-								expertise lies in React.js, Next.js, Angular,TypeScript,
-								Redux-Toolkit, Express.js, Mongoose, Tailwind CSS and .Net,
-								Oracle.
-							</p>
-							<p className="text-muted-foreground leading-relaxed max-w-2xl">
-								I started my journey with a degree in Computer Science, then
-								expanded my skills as a self-taught programmer, constantly
-								pushing the boundaries of modern web development. I enjoy
-								crafting scalable, dynamic applications, solving complex
-								problems, and staying up-to-date with the ever-evolving tech
-								landscape.
-							</p>
-							<p className="text-muted-foreground leading-relaxed max-w-2xl">
-								When I’m not coding, I’m usually reading, exploring new places,
-								or enjoying a great cup of coffee. I believe in continuous
-								learning and am always excited to take on new challenges and
-								collaborations!
-							</p>
-						</div>
+  return (
+    <section className="relative md:pt-10 md:pb-56 2xl:pb-28 dark:bg-darkTheme">
+      <div className="hidden md:block">
+        <Spotlight
+          className="-top-40 left-0 md:left-60 md:top-40"
+          fill="white"
+        />
+      </div>
+      <div className="hidden md:block">
+        <TextHoverEffect text="ABOUT" />
+      </div>
+      <div className="w-full md:absolute md:top-0 lg:translate-y-[50%]">
+        <div className="flex justify-center">
+          <div className="md:col-span-4 space-y-6 text-center md:text-left">
+            <div className="space-y-4 flex flex-col items-center text-center">
+              <h1 className="font-bold text-4xl md:text-5xl pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-teal-900 to-teal-400 bg-clip-text leading-none text-transparent dark:from-white dark:to-slate-900/10">
+                About Me
+              </h1>
+              <p className="text-muted-foreground leading-relaxed max-w-2xl">
+                I am a passionate Full-Stack Developer with a strong focus on
+                building responsive, high-performance web applications. My
+                expertise lies in React.js, Next.js, Angular,TypeScript,
+                Redux-Toolkit, Express.js, Mongoose, Tailwind CSS and .Net,
+                Oracle.
+              </p>
+              <p className="text-muted-foreground leading-relaxed max-w-2xl">
+                I started my journey with a degree in Computer Science, then
+                expanded my skills as a self-taught programmer, constantly
+                pushing the boundaries of modern web development. I enjoy
+                crafting scalable, dynamic applications, solving complex
+                problems, and staying up-to-date with the ever-evolving tech
+                landscape.
+              </p>
+              <p className="text-muted-foreground leading-relaxed max-w-2xl">
+                When I’m not coding, I’m usually reading, exploring new places,
+                or enjoying a great cup of coffee. I believe in continuous
+                learning and am always excited to take on new challenges and
+                collaborations!
+              </p>
+            </div>
 
-						<div className="flex justify-center">
-							<ShinyButton>
-								<a
-									className="w-full block"
-									href="/RITTIKA_DEV.pdf"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									Download Resume
-								</a>
-							</ShinyButton>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+            <div className="flex text-center justify-center">
+              <a className="w-full block" href="/RITTIKA_DEV.pdf" download>
+                <ShinyButton>Download Resume</ShinyButton>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AboutMe;
